b9d5439a1eaf03d879a64ecfa4562d75d73deac1
charsDev <chars24@gmail.com>
2021-09-09 21:29:23 -0500

    SWR

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,44 +1,35 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { View, Text } from 'react-native'
+import React from 'react'
+import { Text } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import styled from 'styled-components/native'
+import useSWR from 'swr'
+import { fetcher } from '../commons/utils'
 import client_id from '../env'
 
 const Container = styled.View`
   flex: 1;
   padding-top: ${(props) => props.paddingTop}px;
 `
+const URL = `https://api.unsplash.com/photos/?client_id=${client_id}`
 
 const Home = () => {
-  const URL = `https://api.unsplash.com/photos/?client_id=${client_id}`
-  
-  const [data, setData] = useState([])
 
-  const fetchData = async () => {
-    try {
-      const response = await axios(URL)
-      console.log(response.data)
-      setData(response.data)
-    } catch (error) {
-      console.log("Ocurrio un error", error)
-    }
-  }
-
-  useEffect(() => {
-    fetchData()
-  },[])
-
+  const { data, error } = useSWR(URL, fetcher, {
+    initialData: [],
+    revalidateOnMount: true
+  })
 
   const insets = useSafeAreaInsets();
   return (
     <Container paddingTop={insets.top}>
       <Text>Home</Text>
-      {React.Children.toArray(        
-        data.map((item) => {
-          return <Text>{item.id}</Text>
+      {!!data &&
+        React.Children.toArray(        
+          data.map((item) => {
+            return <Text>{item.id}</Text>
         })
       )}
+      {!!error && <Text>Ocurrio un error</Text>}
     </Container>
   )
 }
